4f0071d70c0b0b3c2e844b6f263d54194c8059b6
Rohit T P <tprohit9@gmail.com>
2023-01-17 14:05:05 +0530

    refactor: fixed eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [1, 'always', ['ui', 'real']],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'chore',
-        'style',
-        'refactor',
-        'ci',
-        'test',
-        'perf',
-        'revert',
-      ],
-    ],
-  },
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "scope-enum": [1, "always", ["ui", "real"]],
+        "type-enum": [
+            2,
+            "always",
+            [
+                "feat",
+                "fix",
+                "docs",
+                "chore",
+                "style",
+                "refactor",
+                "ci",
+                "test",
+                "perf",
+                "revert",
+            ],
+        ],
+    },
 };
