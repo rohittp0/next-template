7d395e571037fd2d9c091883d99224c4a42c0087
Rohit T P <tprohit9@gmail.com>
2023-01-31 15:21:43 +0530

    refactor: moved to pnpm

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,10 +2,10 @@
  * @type {import('next-sitemap').IConfig}
  */
 module.exports = {
-  // Without additional '/' on the end
-  siteUrl: 'https://rohittp.com',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
+    // Without additional '/' on the end
+    siteUrl: "https://rohittp.com",
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [{ userAgent: "*", allow: "/" }],
+    },
 };
